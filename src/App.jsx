e28289348bd2608990cb37ffe6f589079d4652f5
GregoryVDev <arexo@hotmail.fr>
2024-06-05 09:31:02 +0200

    Moved the TodoList.jsx file to the components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import "./App.css";
-export const HelloWorld = ({ bienvenue }) => {
-  return <h1>{bienvenue}</h1>;
-};
 
 export const App = () => {
-  return (
-    <div>
-      <HelloWorld bienvenue="Test" />
-    </div>
-  );
+  return <div></div>;
 };

cd177e0c2419223a82ea28f5637e2fc9585e1c0f
GregoryVDev <arexo@hotmail.fr>
2024-06-05 15:11:34 +0200

    Added arrow const ‘Input’

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 export const App = () => {
   const handleClick = () => {
@@ -11,7 +12,7 @@ export const App = () => {
       <Button
         onClick={handleClick}
         style={{ backgroundColor: "blue", color: "white" }}
-        added="Add"
+        added="Add Task"
       />
     </div>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,1 +1,33 @@
-export const TodoList = () => {};
+import { useState } from "react";
+
+export const TodoList = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor Appointment",
+      completed: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      completed: false,
+    },
+  ]);
+  const [text, setText] = useState("");
+
+  const AddTask = (text) => {
+    const newTask = {
+      id: 1,
+      text,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+    setText("");
+  };
+
+  const DeleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleCompleted = (id) => {};
+};
